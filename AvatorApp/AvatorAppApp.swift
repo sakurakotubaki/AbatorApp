858a1c8a018c993a045b409c949bd3e6136c736a
sakurakotubaki <gitonly543@gmail.com>
2023-12-16 23:33:04 +0900

    Initial Commit

---
+++ AvatorApp/AvatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvatorAppApp.swift
+//  AvatorApp
+//
+//  Created by 橋本純一 on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct AvatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
